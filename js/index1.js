0874cf2c07f2e2bd1d5347a3ec7912b0f4107a6b
wuchen <603508769@qq.com>
2017-08-19 09:03:11 +0800

    hq3

---
+++ js/index1.js
@@ -0,0 +1,8 @@
+var btn1 = document.querySelector(".btn1");
+var btn2 = document.getElementsByTagName("button")[1];
+btn1.onclick=function(){
+	location.href="login.html"
+}
+btn2.onclick=function(){
+	location.href="register.html"
+}
